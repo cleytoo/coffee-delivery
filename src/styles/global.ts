458c15c417c9944c6e1771440896fab4f5c1e4ca
cleyton vinicius <cleytonterceiro@outlook.com>
2022-09-11 15:12:08 -0300

    🚧 configurações do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background-color: ${(p) => p.theme.background};
+    color: ${(p) => p.theme['base-subtitle']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
+
+// font-family: 'Baloo 2', cursive;
